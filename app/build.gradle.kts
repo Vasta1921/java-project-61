8c927667a2f5cc27efaf12de2eb69791862cf7ee
dimonpokemon16 <kostin.dima16@gmail.com>
2026-01-26 16:48:01 +0300

    2 шаг

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,16 @@
 plugins {
-    id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
+    id ("application")
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
 
 repositories {
     mavenCentral()
@@ -14,6 +20,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 tasks.test {
